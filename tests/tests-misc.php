d720bd706fb6ca500ce6b4a3c2ac0af188ad4a28
Ghost1227 <dgriffiths@section214.com>
2015-03-26 23:36:00 -0500

    Update unit tests

--- tests/tests-misc.php
+++ tests/tests-misc.php
@@ -15,4 +15,16 @@ class Test_Misc extends WP_UnitTestCase {
         $this->assertArrayHasKey( 'post', smartview_get_types() );
         $this->assertArrayHasKey( 'page', smartview_get_types() );
     }
+
+    public function test_get_title_tags() {
+        $this->assertStringStartsWith( '<div class="smartview-tag-list">', smartview_get_title_tags() );
+        $this->assertStringEndsWith( '</div>', smartview_get_title_tags() );
+    }
+
+    public function test_parse_title_tags() {
+        $content = 'Test {sitename}';
+        $parsed_content = 'Test sitename';
+
+        $this->assertEquals( $parsed_content, smartview_parse_title_tags( $content ) );
+    }
 }
